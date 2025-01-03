db177b83ab2dea71a201592cbcdfb97da4959c66
Wendy-Patricia <wendypatriciam2018@gmail.com>
2025-01-03 01:29:53 +0100

    creation of some locations

--- location.c
+++ location.c
@@ -5,18 +5,22 @@
 
 extern Location *LocationNew(char *name, char *desc)
 {
-   Location *ret = (Location *)NULL;
+   Location *loc = (Location *)NULL;
    if (name && desc) /** && where)**/
    {
-      ret = malloc(sizeof(Location));
-      if (ret)
+      loc = malloc(sizeof(Location));
+      if (loc)
       {
-         ret->name = strdup(name);
-         ret->desc = strdup(desc);
+         loc->name = strdup(name);
+         loc->desc = strdup(desc);
+         for (int i = 0; i < 6; i++)
+         {
+            loc->exits[i] = NULL; // everthing  is inicializated with null with no incial location
+         }
       }
-      // ret->currentLocation = NULL;
+      // loc->currentLocation = NULL;
    }
-   return ret;
+   return loc;
 }
 
 extern void LocationDelete(Location *l)
@@ -32,8 +36,73 @@ extern void LocationDelete(Location *l)
    return (Location *)NULL;
 }
 
-extern void LocationPrint(Location *l)
+void LocationPrint(Location *loc)
 {
-   if (l)
-      printf("%s\n%s\n", l->name, l->desc);
+   if (loc)
+   {
+      printf("Location: %s\n", loc->name);
+      printf("Description: %s\n", loc->desc);
+      printf("Exits:\n");
+      const char *directions[] = {"NORTH", "EAST", "SOUTH", "WEST", "UP", "DOWN"};
+      for (int i = 0; i < 6; i++)
+      {
+         if (loc->exits[i])
+         {
+            printf("- %s: %s\n", directions[i], loc->exits[i]->name);
+         }
+      }
+   }
+}
+
+/* Define connexion between 2 locations*/
+void LocationSetExit(Location *from, Direction dir, Location *to)
+{
+   if (from && dir >= 0 && dir < 6)
+   {
+      from->exits[dir] = to;
+   }
+}
+
+/* creation of location" */
+Location *LocationInit()
+{
+   Location *loc1 = LocationNew("On the road", "You are in a open field with only beautiful flowers around you.");
+   Location *loc2 = LocationNew("On the road", "You are in a open field with only beautiful flowers around you.");
+   Location *loc3 = LocationNew("On the road", "You are in a open field with only beautiful flowers around you.");
+   Location *loc4 = LocationNew("FrontYard", "A long hallway with doors on both sides.");
+ 
+
+   // Connection
+   LocationSetExit(loc1, NORTH, loc2);
+   LocationSetExit(loc2, SOUTH, loc1);
+   LocationSetExit(loc2, NORTH, loc3);
+   LocationSetExit(loc3, SOUTH, loc2);
+
+   return loc1; // Retorne the first location
+}
+
+
+void LocationDestroy(Location *start)
+{
+   if (!start)
+      return;
+
+   Location *visited[100] = {NULL};
+   int count = 0;
+
+   visited[count++] = start;
+
+   for (int i = 0; i < count; i++)
+   {
+      Location *current = visited[i];
+      for (int j = 0; j < 6; j++)
+      {
+         Location *next = current->exits[j];
+         if (next && next != start)
+         {
+            visited[count++] = next;
+         }
+      }
+      LocationDelete(current);
+   }
 }
--- location.h
+++ location.h
@@ -1,16 +1,21 @@
 #ifndef LOCATION_H
 #define LOCATION_H
+#include "exits.h"
 
 
 typedef struct 
 {
     char *name;
     char *desc;
+    Location*exits[6]; //for each direction(north, south, east, west, up and down)
 } Location;
 
 extern Location *LocationNew(char *name, char *desc);
 extern void LocationDelete(Location *l);
 extern void LocationPrint(Location *l);
+void LocationSetExit(Location *from, Direction dir, Location *to);
+Location *LocationInit();//allocate the new locationd
+void LocationDestroy(Location *start);//destroy the world
 
 
 #endif // LOCATION_H
